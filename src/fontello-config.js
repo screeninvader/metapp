a4fabad2879f5196a19aeb60f79bbedd886b279a
Jascha Ehrenreich <jascha@jaeh.at>
2015-03-20 05:49:35 +0100

    added fontello, niceified bin/install otw

---
+++ src/fontello-config.js
@@ -0,0 +1,16 @@
+{
+  "name": "metapp",
+  "css_prefix_text": "i-",
+  "css_use_suffix": false,
+  "hinting": true,
+  "units_per_em": 1000,
+  "ascent": 850,
+  "glyphs": [
+    {
+      "uid": "1e32e1f8787cf3cb9f52f8850822dcb6",
+      "css": "cogs",
+      "code": 59392,
+      "src": "elusive"
+    }
+  ]
+}
