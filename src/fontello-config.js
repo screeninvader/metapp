fd8db45fb8742579e4e1ccf3e4b5d92b8f2a2e41
Jascha Ehrenreich <jascha@jaeh.at>
2015-03-21 02:31:06 +0100

    add fontello-cli and uglify to build

--- src/fontello-config.js
+++ src/fontello-config.js
@@ -7,9 +7,21 @@
   "ascent": 850,
   "glyphs": [
     {
+      "uid": "7eed700d710c276569f95be26326fa5b",
+      "css": "heart-empty",
+      "code": 59393,
+      "src": "elusive"
+    },
+    {
       "uid": "1e32e1f8787cf3cb9f52f8850822dcb6",
       "css": "cogs",
       "code": 59392,
+      "src": "elusive"
+    },
+    {
+      "uid": "599d6c088f4a1c64611b989459d65f6b",
+      "css": "lightbulb",
+      "code": 59394,
       "src": "elusive"
     }
   ]
